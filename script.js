eb1f8eb12c689b3fb41e2c379a36dd315450ce9e
Luba Pecheneva <lp5786766@gmail.com>
2022-07-23 13:05:23 -0400

    added mobile page

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+  location.href = 'mobile.html';
+}
+
+
 const triangle = $('#tri');
 
+// triangle.click(() => {
+//     alert("Hello!");
+// });
 
-triangle.click(() => {
-    alert("Hello!");
-});
+function printMousePos(event) {
+  console.log('clientX: ' + event.clientX + ' - clientY: ' + event.clientY);
+}
+
+document.addEventListener('click', printMousePos);
